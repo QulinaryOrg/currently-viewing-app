0a6638170d83b72670f23584b0e5399c8e4f2e4b
FoysalOsmany <foysalo@gmail.com>
2016-12-01 19:20:19 +0600

    viewers pipe

---
+++ client/src/app/components/viewers/viewers.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ViewersPipe } from './viewers.pipe';
+
+describe('ViewersPipe', () => {
+  it('create an instance', () => {
+    let pipe = new ViewersPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
