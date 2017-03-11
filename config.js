42db81f3b9f19a996b245ff69a56e6ae60b89f43
Arvind Kala <arvind.kala@bigdecisions.com>
2017-03-12 01:55:11 +0530

    Set the default view to Mustache

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 config.application_port = 3000
 config.db_user = 'root'
-config.db_pass = 'bigdecision'
+config.db_pass = 'root'
 config.db_name = 'taro_interview'
 config.table_name = 'user_ip_address'
 
