f8303066dff9fa2d971a1931030f0dfc753cf9fc
Vincent <vincent.ml@gmail.com>
2018-04-15 00:43:32 +0800

    add firebase, add / remove ip address on load / unload

--- components/CurrentlyViewing.js
+++ components/CurrentlyViewing.js
@@ -1,6 +1,17 @@
-export default ({ viewers }) => (
+export default ({ viewers = [] }) => (
   <div>
-    Currently viewing:
-    {viewers.map(viewer => <div key={viewer.ip}>{viewer.ip}</div>)}
+    IP addresses currently viewing this app:
+    {viewers.length === 0 ? (
+      <div>Loading...</div>
+    ) : (
+      <div>
+        {viewers.map(viewer => (
+          <div key={viewer.ip}>
+            {viewer.ip}
+            {viewer.isViewer ? " (you)" : ""}
+          </div>
+        ))}
+      </div>
+    )}
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,58 @@
 import React, { Component } from "react";
 import Head from "next/head";
+import * as firebase from "firebase";
 import ipify from "ipify";
 import CurrentlyViewing from "../components/CurrentlyViewing";
 
 export default class extends React.Component {
-  static async getInitialProps() {
-    const ip = await ipify();
-    return {
-      viewers: [
-        {
-          ip
-        }
-      ]
+  state = {};
+  async componentDidMount() {
+    // credentials hardcoded for demo. In production, api keys should be
+    // placed in .env variables and injected during runtime
+    var config = {
+      apiKey: "AIzaSyAnutkEB8dWgh3sqWGZQZmf67rnrtu5ZKk",
+      authDomain: "qulinary-currently-viewing.firebaseapp.com",
+      databaseURL: "https://qulinary-currently-viewing.firebaseio.com",
+      storageBucket: "qulinary-currently-viewing.appspot.com"
     };
+    // initialize firebase.
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+    // get firebase db instance
+    const firedb = firebase.database();
+    // get client's ip address using ipify api service
+    const ip = await ipify();
+
+    // rerender viewer list
+    this.setState({ ip });
+
+    // on change to viewers, re-render viewers list
+    firedb.ref("viewers").on("value", snapshot => {
+      const data = snapshot.val();
+      const viewers = Object.keys(data).map(key => ({
+        ip: data[key].ip,
+        isViewer: data[key].ip === ip /* to identify this viewer */
+      }));
+      this.setState({ viewers });
+    });
+    // firebase doesn't allow "." as a key, use hyphen instead
+    const urlFriendlyKey = ip.split(".").join("-");
+    // get ref to this viewer
+    const thisViewerRef = firedb.ref(`viewers/${urlFriendlyKey}`);
+    /* update this viewer's IP to firebase */
+    thisViewerRef.set({ ip });
+    /* on disconnect, remove from firebase */
+    thisViewerRef.onDisconnect().remove();
   }
   render() {
-    console.log("viewers:", this.props.viewers);
-
     return (
       <div>
         <Head>
           <title>Qulinary - Currently Viewing</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <CurrentlyViewing viewers={this.props.viewers} />
+        <CurrentlyViewing viewers={this.state.viewers} />
       </div>
     );
   }
