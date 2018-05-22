f3d29c2a91f7db3db3f689fb7c4733737a192cc4
Chijioke Ndubisi <cjndubisi@gmail.com>
2018-05-22 12:04:48 +0100

    chore: refactor when when a connection is closed

--- server/index.js
+++ server/index.js
@@ -18,16 +18,14 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', async (ws, req) => {
     let result = null;
     let ip = null;
-    ws.isAlive = true;
-
 
     const clientAddress = req.connection.remoteAddress;
     const matches = clientAddress.match(/\d{3}.\d{1,3}.\d{1,3}.\d{1,3}/g)
-    console.log(clientAddress, matches);
+
     ip = (matches && matches.length !== 0) ? match[0] : clientAddress;
 
     ws.identifer = ip
-    console.log('ips',ip);
+
     try {
         await appendUsers(ip);
         result = await getConnectedUsers();
@@ -36,17 +34,25 @@ wss.on('connection', async (ws, req) => {
         ws.send({ error, message: 'something went wrong connecting to you' });
         return
     }
-    console.log(result);
 
     wss.clients.forEach((ws) => {
-        if (!ws.isAlive) return ws.terminate();
+        if (ws.readyState === ws.CLOSED) { return ws.terminate(); }
         ws.send(JSON.stringify({ data: result }));
     });
 
+    const token = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.readyState === ws.CLOSED) {
+                ws.terminate();
+            }
+        });
+    }, 30000);
+
     ws.on('close', async () => {
-        ws.isAlive = false;
+        clearInterval(token);
+
         try {
-            console.log('ws id',ws.identifer);
+            console.log('ws id', ws.identifer);
             await removeUser(ws.identifer);
             result = await getConnectedUsers();
         } catch (error) {
@@ -54,7 +60,7 @@ wss.on('connection', async (ws, req) => {
         }
 
         wss.clients.forEach((ws) => {
-            if (!ws.isAlive) return ws.terminate();
+            if (ws.readyState === ws.CLOSED) ws.terminate();
             ws.send(JSON.stringify({ data: result }));
         });
     })
